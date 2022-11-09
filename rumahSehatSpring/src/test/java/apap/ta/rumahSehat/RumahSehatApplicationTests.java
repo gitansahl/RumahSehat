53537126390693b2f04c4d17f1563bde7b0db242
Gitan Sahl Wijaya <gitan.sahl@ui.ac.id>
2022-11-09 10:50:54 +0700

    init spring

---
+++ rumahSehatSpring/src/test/java/apap/ta/rumahSehat/RumahSehatApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.ta.rumahSehat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RumahSehatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
