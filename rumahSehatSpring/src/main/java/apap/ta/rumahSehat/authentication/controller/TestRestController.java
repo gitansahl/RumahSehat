26270dc40fa70dec22ad5465add4831ea4d1144a
Gitan Sahl Wijaya <gitan.sahl@ui.ac.id>
2022-11-13 11:16:06 +0700

    feat: auth - fix jwt

--- rumahSehatSpring/src/main/java/apap/ta/rumahSehat/authentication/controller/TestRestController.java
+++ rumahSehatSpring/src/main/java/apap/ta/rumahSehat/authentication/controller/TestRestController.java
@@ -1,9 +1,17 @@
 package apap.ta.rumahSehat.authentication.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/test")
+@RequestMapping("/api")
 public class TestRestController {
+
+    @RequestMapping({ "/hello" })
+    public String firstPage() {
+        SecurityContextHolder.getContext().setAuthentication(null);
+        return "Hello World";
+    }
 }
