437cdc9fc39b25c5a22e3591af91bd1eb11b58a9
josias.marchellino <josias.marchellino@ui.ac.id>
2022-11-23 21:41:27 +0700

    update2:fitur melihat daftar obat

--- rumahSehatSpring/src/main/java/apap/ta/rumahSehat/obat/controller/ObatController.java
+++ rumahSehatSpring/src/main/java/apap/ta/rumahSehat/obat/controller/ObatController.java
@@ -4,6 +4,7 @@ package apap.ta.rumahSehat.obat.controller;
 import apap.ta.rumahSehat.obat.model.ObatModel;
 import apap.ta.rumahSehat.obat.service.ObatService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +18,16 @@ public class ObatController {
     private ObatService obatService;
 
     @GetMapping("/obat")
-    private String viewallObat(Model model){
-        List<ObatModel> listObat = obatService.getListObat();
-        model.addAttribute("listObat", listObat);
-        return "obat/viewall-obat";
+    private String viewallObat(Authentication authentication, Model model){
+        if(authentication.getAuthorities().stream().anyMatch(a->a.getAuthority().equals("Admin"))
+        || authentication.getAuthorities().stream().anyMatch(a->a.getAuthority().equals("Apoteker"))){
+            List<ObatModel> listObat = obatService.getListObat();
+            model.addAttribute("listObat", listObat);
+            return "obat/viewall-obat";
+        }
+        else {
+            return "error/403";
+        }
     }
 
 
